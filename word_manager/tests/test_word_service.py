5bc9074d29232c0617b8296a71453a83ac5f6762
canasmh <canasmh@yahoo.com>
2023-11-20 18:39:50 -0500

    Modify current word_service test
    The test now pulls the target language from env and verifies the correct language is stored

--- word_manager/tests/test_word_service.py
+++ word_manager/tests/test_word_service.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.conf import settings
 from ..services.word_service import extract_word, store_word
 from ..models import Word
+import os
 
 
 class ExtractWordTests(TestCase):
@@ -26,18 +28,24 @@ class ExtractWordTests(TestCase):
 
 class StoreWordTests(TestCase):
 
+    def setUp(self):
+        settings.TARGET_LANGUAGE = "Spanish"
+
+    def tearDown(self):
+        del settings.TARGET_LANGUAGE
+
     # word is stored successfully
     def test_store_word_success(self):
         response_text = "Word: Cat (Ka-tuh)"
-        store_word(response_text, language="Japanese")
+        store_word(response_text)
         saved_word = Word.objects.last()
         self.assertIsNotNone(saved_word)
         self.assertEqual(saved_word.word, "Cat")
-        self.assertEqual(saved_word.language, "Japanese")
+        self.assertEqual(saved_word.language, "Spanish")
 
     # no word exists
     def test_store_word_no_word(self):
         response_text = "No word here"
-        store_word(response_text, language="Japanese")
+        store_word(response_text)
         saved_word = Word.objects.last()
         self.assertIsNone(saved_word)
